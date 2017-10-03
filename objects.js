674a1fc6b5115ab9e2d614f1f2ccc1435957899b
Chris Nunez <cenunez47@gmail.com>
2017-10-03 18:23:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { rkelly: 'Down Low'};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
